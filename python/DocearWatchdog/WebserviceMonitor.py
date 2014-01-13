2283afe78df843cc63fc545f976f579f7be3adaa
stlanger <langer.st@gmail.com>
2014-01-13 18:44:50 +0100

    test if graphdb is available
    and send error email if it is not

--- python/DocearWatchdog/WebserviceMonitor.py
+++ python/DocearWatchdog/WebserviceMonitor.py
@@ -5,10 +5,12 @@ Created on Feb 3, 2013
 @author: stefan
 '''
 from functools import wraps
+from email.mime.text import MIMEText
 import errno
 import os
 import signal
 import urllib2
+import smtplib
 
 class TimeoutError(Exception):
     pass
@@ -21,7 +23,7 @@ def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
         def wrapper(*args, **kwargs):
             signal.signal(signal.SIGALRM, _handle_timeout)
             signal.alarm(seconds)
-            try:
+            try:                
                 result = func(*args, **kwargs)
             finally:
                 signal.alarm(0)
@@ -31,9 +33,49 @@ def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
 
     return decorator
 
+@timeout(5, os.strerror(errno.ETIMEDOUT))
+def _test_graphdb():
+    error_msg = ''
+    try:
+        resp = urllib2.urlopen("http://localhost:7474")
+    except:
+        error_msg += '- GraphDB is not available at http://localhost:7474 on fks01!' + os.linesep
+        print('- GraphDB is not available at http://localhost:7474 on fks01!' + os.linesep)
+    
+    if not os.path.exists('/home/stefan/work/mindmap-parser'):
+        error_msg += '- The folder md02:/home/stefan/work is not mounted at fks01:/home/stefan/work!' + os.linesep
+        print('- The folder md02:/home/stefan/work is not mounted at fks01:/home/stefan/work!' + os.linesep)
+        
+    if len(error_msg) > 0:
+        send_graph_db_not_available_email(error_msg)
+    
+        
+def send_graph_db_not_available_email(error_msg):
+    text = '''Recommendations cannot be computed and new mind maps cannot be indexed right now! The reason(s) ''' + os.linesep + os.linesep + error_msg 
+        
+    msg = MIMEText(text)    
+    msg['Subject'] = 'GraphDB not available warning'
+    me = 'WebserviceMonitor@docear.org'
+    msg['From'] = me
+    #to = 'core@docear.org'
+    to = 'core@docear.org'
+    msg['To'] = to
+    
+    smtp_host = 'mail.ovgu.de'
+    smtp_port = 587
+    server = smtplib.SMTP()
+    server.connect(smtp_host,smtp_port)
+    server.ehlo()
+    server.starttls()
+    server.login("genzmehr", "FarbKopiererV49")    
+    
+    server.sendmail(me, to, msg.as_string())
+    server.quit()
+    
+
 @timeout(2, os.strerror(errno.ETIMEDOUT))
 def _test_webservice():
-    resp = urllib2.urlopen("https://api.docear.org/applications/docear/versions/latest").read()    
+    resp = urllib2.urlopen("https://api.docear.org/applications/docear/versions/latest").read()
     if 'version' in resp:
         pass
     else:
@@ -41,13 +83,15 @@ def _test_webservice():
 
 def _restart_webservice():
     os.system("/root/tomcat_restart.sh")
-        
+
 
 def _main():
+    _test_graphdb()
+    
     try:
-        _test_webservice()
+        _test_webservice()        
     except:
         _restart_webservice()
-
+    
 if __name__ == '__main__':
     _main()
